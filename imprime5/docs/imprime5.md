b8f1f76d9a791de2d1133f11f4118320026d5570
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2026-03-24 19:12:14 -0300

    Add files via upload

---
+++ imprime5/docs/imprime5.md
@@ -0,0 +1,16 @@
+# Documentação
+
+
+Programa imprime5
+Descrição: Este programa que imprima os numeros inteiros de 1 a 5.
+Autor: Nelson
+Data: 24/03/2026
+Versão 0.0.1
+
+## Introdução
+
+
+## Funcionalidade
+
+
+## Conclusão
---
+++ imprime5/imprime5/imprime5_001.py
@@ -0,0 +1,29 @@
+"""
+Programa imprime5
+Descrição: Este programa que imprima os numeros inteiros de 1 a 5.
+Autor: Nelson
+Data: 24/03/2026
+Versão 0.0.1
+"""
+
+# Alocação de memória
+
+i = 0
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+
+# Saída de dados
+
+while i < 5:
+    print(i+1)
+    i=i+1
+
+
+
+
+
+
